3597cf405032876eb84b876c6861019f92ccf97c
Akdev Saha <akdevsaha2003@gmail.com>
2025-07-18 21:56:31 +0530

    update

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,15 @@
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
 import { prisma } from "./server/prisma"
+import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [
-
+        GitHub
     ],
+    pages: {
+        signIn: "/login",
+        signOut: "/signup"
+    }
 })
