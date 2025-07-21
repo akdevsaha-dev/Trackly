7349b0930afc2db8c9f46913769cc3b53d8a7823
Akdev Saha <akdevsaha2003@gmail.com>
2025-07-21 22:16:13 +0530

    feat(site): add createSide with full session check, limit

---
+++ src/server/trpc/routers/site.ts
@@ -0,0 +1,46 @@
+import { TRPCError } from "@trpc/server";
+import { procedure, router } from "../trpc";
+import { z } from "zod"
+const MAX_SITES = {
+    FREE: 2,
+    PAID: 20
+}
+export const siteRouter = router({
+    createSite: procedure.input(z.object({
+        url: z.string()
+    }))
+        .mutation(async ({ input, ctx }) => {
+            const session = ctx.session
+            if (!session || !session.user) {
+                throw new TRPCError({ code: "UNAUTHORIZED" })
+            }
+            const userId = session.user.id
+            const user = await ctx.prisma.user.findUnique({
+                where: {
+                    id: userId
+                },
+                include: {
+                    sites: true
+                }
+            });
+            if (!user) {
+                throw new TRPCError({ code: "UNAUTHORIZED" })
+            }
+            const plan = user?.plan;
+            const maxSites = plan === "PAID" ? MAX_SITES.PAID : MAX_SITES.FREE;
+            if ((user?.sites.length ?? 0) >= maxSites) {
+                throw new TRPCError({
+                    code: "FORBIDDEN",
+                    message: `You can monitor upto ${maxSites} with the ${plan.toLowerCase()} plan`
+                })
+            }
+
+            const site = await ctx.prisma.site.create({
+                data: {
+                    url: input.url,
+                    userId
+                }
+            })
+            return site;
+        })
+})
