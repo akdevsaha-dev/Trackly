8d3445c99f9a007f2998e6f464fb372d262b4213
Akdev Saha <akdevsaha2003@gmail.com>
2025-12-09 23:37:41 +0530

    fix: update navbar to suppport responsiveness

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,14 @@
-import { ArrowUpRight } from "lucide-react";
+import { ArrowUpRight, Menu } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <div className="fixed z-50 grid h-20 w-full grid-cols-12 bg-white/1 shadow-md backdrop-blur-lg">
-      <div className="col-span-3 flex items-center pl-13">
-        <Image src={"/logo.svg"} alt="logo" height={100} width={100} />
+    <div className="fixed z-50 flex h-20 w-full items-center justify-between bg-white/1 px-6 shadow-md backdrop-blur-lg md:grid md:grid-cols-12">
+      <div className="flex items-center md:col-span-3">
+        <Image src="/logo.svg" alt="logo" height={50} width={50} />
       </div>
-      <div className="col-span-6 flex items-center justify-center gap-6 text-sm">
+      <div className="hidden items-center justify-center gap-6 text-sm md:col-span-6 md:flex">
         {["Use Cases", "Pricing", "Integration", "Support"].map((item) => (
           <Link
             href={`/${item.toLowerCase().replace(" ", "-")}`}
@@ -19,16 +19,27 @@ export const Navbar = () => {
           </Link>
         ))}
       </div>
-      <div className="col-span-3 flex items-center justify-center gap-10 text-sm">
-        <Link href={"/login"} className="hover:text-neutral-400">
+
+      <div className="hidden items-center justify-center gap-10 text-sm md:col-span-3 md:flex">
+        <Link href="/login" className="hover:text-neutral-400">
           Log in
         </Link>
         <Link
-          href={"/signup"}
+          href="/signup"
           className="flex items-center gap-2 underline underline-offset-8 hover:text-neutral-200"
         >
           <div>Sign up</div>
           <ArrowUpRight size={20} />
+        </Link>
+      </div>
+
+      <div className="flex items-center gap-4 md:hidden">
+        <Link
+          href="/signup"
+          className="flex items-center gap-1 text-sm underline underline-offset-4"
+        >
+          <div>Sign up</div>
+          <ArrowUpRight size={16} />
         </Link>
       </div>
     </div>
